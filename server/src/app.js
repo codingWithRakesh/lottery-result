3c30a986c853fd72131647e562b0930b27983e59
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-10-26 20:57:55 +0530

    feat: initialize server with Express, MongoDB connection, and basic middleware
    - Added package.json for server dependencies and scripts
    - Created app.js to set up Express server with CORS, Helmet, and cookie-parser
    - Implemented MongoDB connection in db.js
    - Defined constants for database name and cookie options
    - Added .gitkeep files for controllers, middlewares, models, routes, and utils directories

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet";
+import cookieParser from "cookie-parser"
+
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true
+}))
+app.use(helmet());
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "working" })
+})
+
+
+export { app }
---
+++ server/src/constants.js
@@ -0,0 +1,6 @@
+export const DB_NAME = "lotteryDB";
+export const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None'
+}
