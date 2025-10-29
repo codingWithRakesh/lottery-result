ea5a66bbb4edc51f8d5ca0afa598d0ca6d2eba32
souptik <souptikbiswas429@gmail.com>
2025-10-29 22:48:34 +0530

    add minor fixes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -5,11 +5,25 @@ const router= Router()
 
 
 router.route("/register").post(registerUser)
+// on ("/api/v1/admin/register")
+// INPUT:- { email, fullname, password }
+
 router.route("/login").post(loginUser)
+// on ("/api/v1/admin/login")
+// INPUT:- { email, password}
+
 router.route("/currentuser").get(verifyJWT,getCurrentUser)
+// on ("/api/v1/admin/currentuser")
+
 router.route("/logout").get(verifyJWT,logoutUser)
+// on ("/api/v1/admin/logout")
+
 router.route("/refresh").get(refreshAccessToken)
+// on ("/api/v1/admin/refresh")
+
 router.route("/changepassword").post(verifyJWT,changeCurrentPassword)
+// on ("/api/v1/admin/changepassword")
+// INPUT:- { email, fullname, password, }
 
 
 export default router
